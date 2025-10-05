2356baaf33569f17441d24f211881ac2d20c1117
pramod ade <prmdextracthere@gmail.com>
2025-10-05 16:53:25 +0530

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,40 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
+  const [input, setInput] = useState("");
+  const [todo, SetTodo] = useState([]);
+
+  function handleClick() {
+    SetTodo([...todo, input]);
+    setInput('');
+  }
+
+  function handleDelete(id) {
+    SetTodo(todo.filter((t,ind) => ind!==id))
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>To-Do List</h1>
+      <div>
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button onClick={handleClick}>Add</button>
+      </div>
+      <div>
+        {todo.map((t,index) => {
+          return (
+            <div key={index}>
+              {t} <button onClick={()=>handleDelete(index)} style={{marginleft:"100px"}}>Delete</button>{" "}
+            </div>
+          );
+        })}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
